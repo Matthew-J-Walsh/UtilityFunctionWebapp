fbbdf37e723bb0924001da7db4f55c28db2c24aa
Matthew <matthewwalsh2021@u.northwestern.edu>
2017-08-17 00:36:39 -0500

    updating first version of nicer compiler

--- applicationCompiler.py
+++ applicationCompiler.py
@@ -3,6 +3,7 @@ import shutil as sh
 import re as re
 import subprocess as sp
 import os
+import argparse
 
 DataFolderLoc = "D:/Someting memey/DataFolder" #this should be a setable thing
 
@@ -20,3 +21,9 @@ def main(progLoc):
     print(uc.Fold(help, filename))
     sh.copyfile(progLoc, NewLocationPy)
     sh.copyfile(manifestLocation, NewLocationMani)
+    print("Finished.")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--pyfile", "-f", help=""" The python program you would like to add to the website (this needs to be the full path) """, required=True)
+progLoc = parser.parse_args().pyfile
+main(progLoc)
